3bfc21db250526e060155aaaec39f938928d2a1b
Alex French <alexcfrench@gmail.com>
2023-09-11 16:46:22 +0100

    fix: updating release to 9

--- provider.tf
+++ provider.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "ResourceGroup-Release-1.0.8"
+  name     = "ResourceGroup-Release-1.0.9"
   location = "UK South"
 }
 
