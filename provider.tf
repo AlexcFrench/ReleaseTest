05f3b582e5ff3bba85e9190e705bf56f7a7c3b73
Alex French <alexcfrench@gmail.com>
2023-09-11 16:26:50 +0100

    feat: added 1.0.4

--- provider.tf
+++ provider.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "ResourceGroup-Release-1.0.3"
+  name     = "ResourceGroup-Release-1.0.4"
   location = "UK South"
 }
 
