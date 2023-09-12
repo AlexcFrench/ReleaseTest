1f2ec6a0245038659f730f526ba867b5d298b0d7
Alex French <alexcfrench@gmail.com>
2023-09-12 16:10:25 +0100

    fix: pub version A

--- provider.tf
+++ provider.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
 
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = join("-", [var.environment, "ResourceGroup-Release"])
+  name     = join("-", [var.environment, "ResourceGroup-Release","A"])
   location = "UK South"
 }
 
