ab0eed0dd1a796fc4c369d01efc2a2e2500a097c
Alex French <alexcfrench@gmail.com>
2023-09-14 11:28:10 +0100

    fix: Release of branch 4 - C

--- BuildResources.tf
+++ BuildResources.tf
@@ -1,5 +1,5 @@
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = join("-", [var.environment, "ResourceGroup-Release","B"])
+  name     = join("-", [var.environment, "ResourceGroup-Release","C"])
   location = "UK South"
 }
