630a29fe62795294ff854b4cfbfe11a068b4ae81
Alex French <alexcfrench@gmail.com>
2023-09-18 17:24:03 +0100

    fix: repo name

--- BuildResources.tf
+++ BuildResources.tf
@@ -13,9 +13,13 @@ data "github_repository" "ThisGHRepo" {
   full_name = data.tfe_workspace.ThisRepoWorkspace.vcs_repo[0].identifier
 }
 
+locals {
+  GHowner = split("/", data.github_repository.ThisGHRepo.full_name)
+}
+
 data "github_release" "latestGHRelease" {
   repository  = data.github_repository.ThisGHRepo.full_name
-  owner       = "alexcfrench@gmail.com"
+  owner       = local.GHowner[0]
   retrieve_by = "latest"
 
 }
